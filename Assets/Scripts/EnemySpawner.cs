db6f1fcee891d17fd82bde6a877aad78ae7f19aa
MAranich <114926980+MAranich@users.noreply.github.com>
2024-05-02 10:23:25 +0200

    Generate enemies away from other entities

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -12,10 +12,16 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private float max_spawn_radius = 10f; 
     [SerializeField] private Vector3 spawn_center = Vector3.zero;
     public static List<GameObject> enemy_list = new List<GameObject>();
-    [SerializeField] private int max_enemy_count = 10;
-    [SerializeField] private int min_enemy_count = 0;
-    [SerializeField] private const float fast_spawn_multiplier = 4f;
-    private float time_for_enemy_spawn = 0f; 
+    [SerializeField] private uint max_enemy_count = 10;
+    [SerializeField] private uint min_enemy_count = 0;
+    [SerializeField] private float inter_enemy_spawn_dist = 5f; //distance to allow spawn between enemies
+    [SerializeField] private float player_to_enemy_spawn_dist = 10f; //distance to allow spawn between enemies
+    
+    
+    private const float fast_spawn_multiplier = 4f;
+    private float time_for_enemy_spawn = 0f;
+    private Transform player_1; 
+    private Transform player_2;
 
 
     void Start()
@@ -24,6 +30,23 @@ public class EnemySpawner : MonoBehaviour
         {
             Debug.Log("Enemy prefab not setted. "); 
         }
+
+        //get reference of player 1 and 2
+        foreach(GameObject possible_player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            if(possible_player.name == "Player1")
+            {
+                player_1 = possible_player.transform;
+                if (player_2 != null) break; 
+            }
+
+            if (possible_player.name == "Player2")
+            {
+                player_2 = possible_player.transform;
+                if (player_1 != null) break;
+            }
+        }
+
     }
 
     void Update()
@@ -82,6 +105,38 @@ public class EnemySpawner : MonoBehaviour
                 continue; 
             }
 
+            Vector3 coordinate = new Vector3(x * max_spawn_radius, 0, y * max_spawn_radius); // in world space
+            Vector3 displacement; //auxiliar variable
+            int enemy_count = enemy_list.Count;
+            bool reject = false; 
+            for (int i = 0; i < enemy_count; i++)
+            {
+
+                displacement = enemy_list[i].transform.position - coordinate; 
+                if(displacement.sqrMagnitude <= inter_enemy_spawn_dist * inter_enemy_spawn_dist)
+                {
+                    reject = true; 
+                    break;
+                }
+
+            }
+
+            if (reject) continue;
+
+            displacement = player_1.position - coordinate; 
+            if(displacement.sqrMagnitude < player_to_enemy_spawn_dist * player_to_enemy_spawn_dist)
+            {
+                // too near to player 1, reject
+                continue; 
+            }
+
+            displacement = player_2.position - coordinate;
+            if (displacement.sqrMagnitude < player_to_enemy_spawn_dist * player_to_enemy_spawn_dist)
+            {
+                // too near to player 2, reject
+                continue;
+            }
+
             //points are valid, accept
             break; 
 
@@ -95,7 +150,8 @@ public class EnemySpawner : MonoBehaviour
         Vector3 pos = spawn_center + new Vector3(x, 0, y); //I lied. y was z all the time. 
         GameObject new_enemy = Instantiate(enemy_prefab, pos, Quaternion.identity); 
 
-        new_enemy.transform.localScale = Vector3.one * (Random.value * Random.value * 0.3f + 1f); // add tiny change in scale
+        new_enemy.transform.localScale = Vector3.one * ((Random.value * Random.value - 0.5f) * 0.3f + 1f); 
+        // add tiny change in scale
 
         enemy_list.Add(new_enemy); 
 
